4738c4f9cae4a46b8e4bd13131cce8b48d4dd8b3
Zarley <48512823+ZarIey@users.noreply.github.com>
2023-01-20 12:10:10 +0100

    Add files via upload
    la c'est bon stp

---
+++ parsing.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctardy <ctardy@student.42nice.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/12 07:20:56 by ctardy            #+#    #+#             */
+/*   Updated: 2023/01/17 12:10:50 by ctardy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./includes/philo.h"
+
+int	is_num(char *str)
+{
+	int	i;
+	
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < 48 || str[i] > 57)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int check_errors_set(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc < 5 || argc > 6)
+		return (1);
+	if ((ft_atoi(argv[1]) <= 1) || (ft_atoi(argv[2]) < 1) || (ft_atoi(argv[3]) < 1) || (ft_atoi(argv[4]) < 1))
+		return (1);
+	while (argv[i])
+	{
+		if (is_num(argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int check_errors(int argc, char **argv)
+{
+	if (check_errors_set(argc, argv))
+	{
+		printf ("on a un soucis ! 🚔 \n");
+		return (1);
+	}
+	return (0);
+}
